7de2e6c870f2724243b26cbc36d29b0ed7820d2e
solongee <sgee456@gmail.com>
2021-10-07 22:48:39 -0400

    going to see if path is created or infinite loop generated

--- src/App.js
+++ src/App.js
@@ -3,100 +3,139 @@ import './App.css';
 import Maze from './Maze.js';
 
 function App() {
-  // const mazeArray = [
-  //   [0, 0, 0, 0, 0],
-  //   [1, 0, 1, 0, 0],
-  //   [0, 0, 0, 1, 0],
-  //   [0, 0, 0, 0, 0],
-  //   [0, 1, 0, 0, 1]
-  // ];
+  const mazeArray = [
+    [0, 0, 0, 0, 0],
+    [1, 0, 1, 0, 0],
+    [0, 0, 0, 1, 0],
+    [0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 1]
+  ];
 
-  function createMazeArray(width, height) {
-    let mazeArray = [];
-    let rowArray = [];
-    //generates our rowArray
-    for (let i = 0; i < width; i++) {
-      rowArray.push(1);
-    }
+  function createMazeWallArray(width, height) {
+    let mazeWallArray = [];
+    //let rowArray =[]; and subsequent loop cannot be in here because then a reference to the same rowArray instance is created for each row in the mazeWallArray. This means that if any function changes an item in one row it will change them in all of them
+
+
     //pushes rowArray onto mazeArray
     for (let i = 0; i < height; i++) {
-      mazeArray.push(rowArray);
+      //generates our rowArray, needs to be inside to ensure that all rowArrays are new instances 
+      let rowArray = [];
+      for (let i = 0; i < width; i++) {
+        rowArray.push(1);
+      }
+      mazeWallArray.push(rowArray);
     }
 
-    return mazeArray;
+    return mazeWallArray;
   };
-      
-  const mazeArray = createMazeArray(20, 20);
 
-  const createPath = function(mazeArray) {
+  const createPath = function() {
     const pathStack = [];
     const startPosition = [0, 0];
+    const newPathArray = createMazeWallArray(5 , 5);
 
     //checks if a square is beside a path
     function checkPathAround(position) {
       const x = position[0];
       const y = position[1];
       //check all eight blocks around possible path block for other path blocks/ outside of maze
-      const onBottomRow = y === mazeArray.length - 1;
+      const onBottomRow = y === newPathArray.length - 1;
       const onTopRow =  y === 0;
       const onLeftColumn = x === 0;
-      const onRightColumn = x === mazeArray[0].length - 1;
+      const onRightColumn = x === newPathArray[0].length - 1;
 
-      //each of these values checks if a square around possible path block is a wall or outside of the maze
+      //each of these values checks if a square around possible path block is a wall or outside of the maze and has a value of true if there's a wall
 
-      const leftValue = onLeftColumn || mazeArray[y][x - 1] === 1;
-      console.log("left is wall: ", leftValue);
+      const leftValue = onLeftColumn || newPathArray[y][x - 1] === 1;
 
-      const rightValue = onRightColumn || mazeArray[y][x + 1] === 1;
-      console.log("right is wall: ", rightValue);
+      const rightValue = onRightColumn || newPathArray[y][x + 1] === 1;
 
-      const bottomValue =  onBottomRow || mazeArray[y + 1][x] === 1;
-      console.log("bottom is wall: ", bottomValue);
+      const bottomValue =  onBottomRow || newPathArray[y + 1][x] === 1;
 
       const bottomLeftValue = 
         onBottomRow || 
         onLeftColumn || 
-        mazeArray[y + 1][x - 1] === 1;
-      console.log("bottom-left is wall: ", bottomLeftValue);
+        newPathArray[y + 1][x - 1] === 1;
       
       const bottomRightValue = 
         onBottomRow || 
         onRightColumn || 
-        mazeArray[y + 1][x + 1] === 1;
-      console.log("bottom-right is wall: ", bottomRightValue);
+        newPathArray[y + 1][x + 1] === 1;
 
               
-      const topValue = onTopRow || mazeArray[y - 1][x] === 1;
-      console.log("top is wall: ", topValue);
+      const topValue = onTopRow || newPathArray[y - 1][x] === 1;
 
       const topLeftValue = 
         onTopRow || 
         onLeftColumn || 
-        mazeArray[y - 1][x - 1] === 1;
-      console.log("top-left is wall: ", topLeftValue);
+        newPathArray[y - 1][x - 1] === 1;
       
       const topRightValue = 
         onTopRow || 
         onRightColumn || 
-        mazeArray[y - 1][x + 1] === 1;
-      console.log("top-right is wall: ", topRightValue);
-
-      
+        newPathArray[y - 1][x + 1] === 1;
     }
     
+    function changeWall(position) {
+      const x = position[0];
+      const y = position[1];
+      newPathArray[y][x] = 0;
+    }
     //start at a wall block, and turn it to path block- push location onto stack
-    mazeArray[0][0] = 1;
+    changeWall(startPosition);
+    pathStack.push(startPosition);
+    while (false && pathStack.length !== 0) {
+      //look at all surrounding blocks for blocks that don't touch another path block
+      const currentPosition = pathStack[pathStack.length -1];
+      const x = currentPosition[0];
+      const y = currentPosition[1];
 
-    while (pathStack.length !== 0) {
+      const possibleTopPosition = [x , y - 1];
+      const possibleBottomPosition = [x , y + 1];
+      const possibleLeftPosition = [x - 1, y];
+      const possibleRightPosition = [x + 1, y];
+
+      const possibleChoices = [];
+
+      if (possibleTopPosition) {
+        possibleChoices.push(possibleTopPosition);
+      }
+      if (possibleBottomPosition) {
+        possibleChoices.push(possibleBottomPosition);
+      }
+      if (possibleLeftPosition) {
+        possibleChoices.push(possibleLeftPosition);
+      }
+      if (possibleRightPosition) {
+        possibleChoices.push(possibleRightPosition);
+      }
+      //randomly choose one to turn into a path block- push location onto stack
+      if (possibleChoices.length !== 0) {
+        const randomIndex = Math.floor(Math.random() * possibleChoices.length);
+        const newPathBlock = possibleChoices[randomIndex];
+        changeWall(newPathBlock);
+        pathStack.push(newPathBlock);
+      } else {
+        pathStack.pop();
+      }
+      //repeat this until no surrounding blocks can be made into path
+      //then pop a block off the stack and try this again
+      //when the stack is gone, the path is complete
 
     }
 
-    //look at all surrounding blocks for blocks that don't touch another path block
-    //randomly choose one to turn into a path block- push location onto stack
-    //repeat this until no surrounding blocks can be made into path
-    //then pop a block off the stack and try this again
-    //when the stack is gone, the path is complete
   }
+
+
+
+  //remove these after testing?
+  //This doesn't work because arrays inside need to be destructured too
+  // const mazeArray = createMazeWallArray(4,4);
+  // const copy = [...mazeArray];
+  // copy[0][0] = "hello";
+  // console.log("mazeWallArray", mazeArray);
+  // console.log("copy", copy)
+
 
   return (
     <div className="App">
